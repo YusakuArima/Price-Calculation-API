2409762058e41a021af8c80030d16d07081f4086
YusakuArima <arima@or-not.com>
2022-06-08 14:58:52 +0900

    料金計算機能完成

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { json } = require('express/lib/response');
+/*const res = require('express/lib/response');
+const { json } = require('express/lib/response');*/
 const app = express();
 const PORT = 3000;
 
+//Json返却
+app.get("/http://localhost:3000/api/cart?product_id=2&amount=1", (req, res) => {
+    res.json({items});
+});
 
 //アプリ内データ
 const arrayObject = [
@@ -12,32 +17,27 @@ const arrayObject = [
 ];
 
 //URLからのIDの取得
-const url = new URL('http://localhost:3000/api/cart?product_id=1&amount=2');
+const url = new URL('http://localhost:3000/api/cart?product_id=2&amount=1');
 const params = new URLSearchParams(url.search);
 const productId = params.get('product_id');
-const amount =params.get('amount');
+const amount =params.get('amount'); 
 
 //引数に商品IDを受け取ると、対応した商品データのObjectを返す関数を書く
 const items = (productID, amount) => {
 //要修正
-    if(productID === undefined){
-        console.log('404')
-    }else if(amount === undefined){
-        console.log('404')
+    if(arrayObject[productID - 1] === undefined){
+        console.log("404");
+    }else if(amount == 0){
+        console.log("404");
     }else{
         console.log(`productName: ${arrayObject[productID - 1].productName}`);
         console.log(`productId: ${productID}`);
-        console.log(`subtotal: ${amount}`)
+        console.log(`subtotal: ${amount}`);
         console.log(`price: ${arrayObject[productID - 1].price*amount}`);
     }
 }
 items(productId,amount);
 
-/*次回：
-下記の実装
-「商品ID、個数どちらかがリクエストに足りない場合、リクエスト時の商品IDに対応する商品がない場合は、レスポンスコード404を返す」
-JSONで返却できるようにする。
-*/
+/*レスポンスコード
+res.sendStatus = (404);*/
 
-
-app.listen(PORT, () =>console.log('OPEN'));
