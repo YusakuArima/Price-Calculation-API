7923fbb12c7009c33175c98a96e11a6b9c678ff7
YusakuArima <arima@or-not.com>
2022-06-14 19:02:09 +0900

    進捗（6/14）

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get("/api/cart", (req, res) => {
+app.get('/api/cart', (req, res) => {
+    // validate params
+    if (req.amount || req.productId ) {
+        return res.sendStatus(404);
+    }
+    // find product
     const arrayObject = [
         { productName: 'T-Shirt', id: '1', price: 4800},
         { productName: 'Jacket', id: '2', price: 19800},
@@ -10,21 +15,16 @@ app.get("/api/cart", (req, res) => {
     ];
     const productId = req.query.product_id;
     const amount =req.query.amount; 
-    const result = arrayObject.find(v => v.id === productId)
-
-    console.log(amount);
-    
-    if (result === undefined || amount === "") {
-    return res.sendStatus(404);
-    }else if(amount === "0"){
-    return res.json(result);
-    }else {
-        result.amount = amount
-        result.subtotal = result.price*amount
+    const result = arrayObject.find(v => v.id === productId);
+    // calc total
+    if(result){
+        result.amount = amount;
+        result.subtotal = result.price*amount;
         return res.json(result);
-    }
-});
+    }else{
+        return res.sendStatus(404);
+    }});
 
-app.listen(PORT, () => console.log("Application started"));
+app.listen(PORT, () => console.log('Application started'));
 
 
